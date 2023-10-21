9235ca51422b90c8f76cb98d6547e8729ab5a7e6
teiwiet <omegalmao1.3@gmail.com>
2023-10-21 12:05:26 +0700

    update

--- function/brute_force.py
+++ function/brute_force.py
@@ -5,7 +5,8 @@ def cal_hash(string):
     hash = hashlib.sha1(string=string.encode())
     pb_hash = hash.hexdigest()
     return pb_hash
-target_hash = "9f6c7c97d4623a6c98a8568e5338cd595e01cc8e"
+target_hash = "4b2214922b397560e09cb40dad5bfff8ded7fcc9"
+# print(len(file))
 for line in lines:
     if cal_hash(line)  == target_hash:
         print(f"Found password : {line}")
--- function/hash.py
+++ function/hash.py
@@ -1,4 +1,4 @@
 import hashlib
-hash_obj = hashlib.sha1("dream123456".encode())
+hash_obj = hashlib.sha1(b"webstudio")
 pb_hash = hash_obj.hexdigest()
 print(pb_hash)
